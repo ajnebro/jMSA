cdf1792f881baab1ff88cdfcd08663a7f14f32ff
clarajvv <61667090+clarajvv@users.noreply.github.com>
2020-05-08 12:02:50 +0200

    primer commit (prueba)

--- src/main/java/org/jmsa/score/impl/Star.java
+++ src/main/java/org/jmsa/score/impl/Star.java
@@ -4,6 +4,7 @@ import org.jmsa.score.Score;
 
 /**
  * Hello world!
+ * grupo 3: irene sanchez, clara jimenez y lucia valverde
  */
 public class Star implements Score {
   public double compute(char[][] sequence) {
