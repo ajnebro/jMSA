0bc30ec35ea8db3568265809e920a8d18c2e6aec
marvalpal <0610727402@uma.es>
2020-05-10 00:50:54 +0200

    Primer commit: Blosum62 con reader interno

--- src/main/java/org/jmsa/substitutionmatrix/impl/Blosum62.java
+++ src/main/java/org/jmsa/substitutionmatrix/impl/Blosum62.java
@@ -2,19 +2,77 @@ package org.jmsa.substitutionmatrix.impl;
 
 import org.jmsa.substitutionmatrix.SubstitutionMatrix;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Hello world!
  */
 public class Blosum62 implements SubstitutionMatrix {
-  public double getDistance(char char1, char char2) {
-    return 0 ;
+
+    private Map<List, Double> substitutionMatrix = new HashMap<List, Double>();
+    private double gapPenalty;
+
+    public Blosum62() throws IOException {
+        this(-8);
+    }
+
+    public Blosum62(double gP) throws IOException {
+        gapPenalty=gP;
+        substitutionMatrix=matrixReader();
+    }
+
+    public double getDistance(char char1, char char2) {
+
+      double result=0;
+      if(char1=='-' & char2 =='-'){
+          result=1;
+      }else if (char1=='-' || char2 =='-'){
+          result=this.getGapPenalty();
+      }else{
+          Map<List, Double> matrix = substitutionMatrix;
+          if (matrix.get(List.of(char1, char2))!=null) {
+              result = matrix.get(List.of(char1, char2));
+          }else{
+              result = matrix.get(List.of(char2, char1));
+          }
+      }
+      return result ;
   }
 
-  public char getGapCharacter() {
+  public char getGapCharacter(){
     return '-' ;
   }
 
   public double getGapPenalty() {
-    return 0 ;
+    return gapPenalty ;
   }
+
+  private Map matrixReader() throws IOException {
+
+
+      String filePath = new File("resources\\data\\Blosum62.txt").getAbsolutePath();
+      Map<List, Double> sMatrix = new HashMap<List, Double>();
+
+      String line;
+      BufferedReader reader = new BufferedReader(new FileReader(filePath));
+      while ((line = reader.readLine()) != null)
+      {
+          String[] parts = line.split(":", 2);
+          if (parts.length >= 2)
+          {
+              String[] lp=parts[0].split(",");
+              List key = List.of(lp[0].charAt(0),lp[1].charAt(0));
+              String value = parts[1];
+              sMatrix.put(key, Double.valueOf(value));
+          } else {
+              System.out.println("ignoring line: " + line);
+          }
+      }
+      reader.close();
+
+        return sMatrix;
+    }
 }
