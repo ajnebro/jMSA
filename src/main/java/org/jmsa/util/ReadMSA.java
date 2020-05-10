08c91808d64b03c52bb29bf52b0b1d8366fa3c15
Rostyslav Zavoiko <rosszavoyko@gmail.com>
2020-05-10 12:59:54 +0300

    Implemented class ReadMSA

--- src/main/java/org/jmsa/util/ReadMSA.java
+++ src/main/java/org/jmsa/util/ReadMSA.java
@@ -1,7 +1,74 @@
 package org.jmsa.util;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReadMSA {
-  public static char[][] readSequenceFromFastaFile() {
-    return new char[0][];
+  static char[][] myArray;
+  public static char[][] readSequenceFromFastaFile(String filepath) throws FileNotFoundException {
+    List<List<Character>> seqList = new ArrayList<List<Character>>();
+    int rowCnt=0;
+
+    File file = new File(filepath);
+    Scanner reader = new Scanner(file);
+
+    boolean first = true;
+    while(reader.hasNextLine()) {
+      String line = reader.nextLine().trim();
+      if (line.isBlank()==false) {
+        if (line.charAt(0)=='>') {
+          List<Character> myList = new ArrayList<Character>();
+          seqList.add(myList);
+          if(first) {
+            first=false;
+          }
+          else {
+            rowCnt++;
+          }
+        }
+        else{
+          char[] secuenceArray = line.toCharArray();
+          for (char c : secuenceArray) {
+            seqList.get(rowCnt).add(c);
+          }
+        }
+      }else { //line.isBlank()==true;
+        if(reader.hasNextLine()) {
+          line=reader.nextLine();
+        }else {
+          line=null;
+        }
+      }
+    }
+
+    reader.close();
+
+    if(!allListsEqual(seqList)) {
+      throw new RuntimeException("The sequences in the input .fasta file are not aligned correctly");
+    }
+
+    myArray = new char[rowCnt+1][seqList.get(0).size()];
+    for (int i=0; i <= rowCnt; i++) {
+      for (int j=0; j<seqList.get(i).size(); j++) {
+        myArray[i][j]=seqList.get(i).get(j);
+      }
+    }
+
+    return myArray;
+  }
+
+  public static boolean allListsEqual(List<List<Character>> theList) {
+    boolean eq = true;
+    int sizeRef = theList.get(0).size();
+    for (List<Character> list : theList) {
+      if(list.size()!=sizeRef) {
+        eq = false;
+      }
+    }
+
+    return eq;
   }
 }
