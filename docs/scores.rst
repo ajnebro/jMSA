bf223d7f059324854efe9bab21911c4806bd430d
Juan Antonio Herrera Conde <jaherreraconde@MacBook-Pro-de-Juan-JR.local>
2020-05-13 12:39:36 +0200

    Entropy's tests and documentation

--- docs/scores.rst
+++ docs/scores.rst
@@ -18,6 +18,12 @@ Percentage of totally conserved columns
 Entropy
 -------
 
-First of all we have to talk about Claude Shannon, known as the information theory's father. He was the inventor of different items
-as the telegraph system. In addition, he defined the Shanon Entropy, which can be:
+Entropy can be related to physics' magnitude that allows us to measure the non-usable part of the energy
+contained in a system of particles or data. That means that that part of the total can not be used to produce
+physical work. It also measures how diverse are the residues in a column. It can also be understood as a way
+of measuring the disorder in a specific point of the matter. Entropy is important because it tells us that systems
+can't go back in their processes and how is the energy wasted in a closed environment. Mathematically it's
+represented by the summatory of residue's frequency times the logarithm of different residue's number.
+
+
 
--- src/test/java/org/jmsa/score/EntropyTest.java
+++ src/test/java/org/jmsa/score/EntropyTest.java
@@ -6,46 +6,147 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- *  implement Test for Entropy
+ * implement Test for Entropy
  * Entropy('A')= 0--> length =1
  * Entropy('AT')=1
  * Entropy('ATCG')=
  * F2->2
  * negative->??
- *
  */
+
 public class EntropyTest {
+
     private Entropy entropy;
     private char[][] sequence;
+
     @BeforeEach
-    public void setup(){
+    public void setup() {
         entropy = new Entropy();
     }
+
     @Test
     public void shouldEntropyOfSequenceOf2NucleotidesReturn0() {
         long expectedValue = 0;
-        sequence= new char['A']['A'];
-       double obtainedValue = entropy.compute(sequence);
+        sequence = new char['A']['A'];
+        double obtainedValue = entropy.compute(sequence);
         assertEquals(expectedValue, obtainedValue);
-
     }
+
+    @Test
+    public void shouldEntropyOfSequenceOf2DifferentNucleotidesReturn2() {
+        long expectedValue = 1;
+        sequence = new char[][]{{'A'}, {'G'}};
+        double obtainedValue = entropy.compute(sequence);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
     @Test
     public void shouldEntropyOfSequenceOf4DifferentNucleotidesReturn2() {
         long expectedValue = 2;
-        sequence= new char[][]{{'A', '-','-','G'}, {'C', 'T'}};
+        sequence = new char[][]{{'A', 'G'}, {'C', 'T'}};
         double obtainedValue = entropy.compute(sequence);
         assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldEntropyOfSequenceOf6EqualNucleotidesReturn1() {
+        long expectedValue = 1;
+        sequence = new char[][]{{'A', 'G'}, {'A', 'G'}};
+        double obtainedValue = entropy.compute(sequence);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldEntropyOfSequenceOf8InvertedNucleotidesReturn2() {
+        long expectedValue = 2;
+        sequence = new char[][]{{'A', 'G', 'C', 'T'}, {'T', 'C', 'G', 'A'}};
+        double obtainedValue = entropy.compute(sequence);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldEntropyOfSequenceOf8NoOrderNucleotidesReturn2() {
+        long expectedValue = 2;
+        sequence = new char[][]{{'A', 'G', 'C', 'T'}, {'G', 'T', 'A', 'C'}};
+        double obtainedValue = entropy.compute(sequence);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldEntropyOfSequenceOf8EqualNucleotidesReturn2() {
+        long expectedValue = 2;
+        sequence = new char[][]{{'A', 'G', 'C', 'T'}, {'A', 'G', 'C', 'T'}};
+        double obtainedValue = entropy.compute(sequence);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldEntropyWith2GapsReturn0() {
+        long expectedValue = 0;
+        sequence = new char[][]{{'_'}, {'_'}};
+        double obtainedValue = entropy.compute(sequence);
+        assertEquals(expectedValue, obtainedValue);
+
+    }
+
+    @Test
+    public void shouldEntropyWith4GapsReturn0() {
+        long expectedValue = 0;
+        sequence = new char[][]{{'_', '_'}, {'_', '_'}};
+        double obtainedValue = entropy.compute(sequence);
+        assertEquals(expectedValue, obtainedValue);
+
+    }
+
+    @Test
+    public void shouldEntropyWith8GapsReturn0() {
+        long expectedValue = 0;
+        sequence = new char[][]{{'_', '_', '_', '_'}, {'_', '_', '_', '_'}};
+        double obtainedValue = entropy.compute(sequence);
+        assertEquals(expectedValue, obtainedValue);
+
     }
 
     @Test
     public void shouldEntropyWithGapsOfSequenceOf2NucleotidesReturn0() {
         long expectedValue = 0;
-        sequence= new char[][]{{'A','-'},{'A'}};
+        sequence = new char[][]{{'A', '-'}, {'A'}};
         double obtainedValue = entropy.compute(sequence);
         assertEquals(expectedValue, obtainedValue);
 
     }
 
+    @Test
+    public void shouldEntropyOfSequenceOf4DifferentNucleotidesWithGapsReturn2() {
+        long expectedValue = 2;
+        sequence = new char[][]{{'A', 'G', '_'}, {'C', 'T', '_'}};
+        double obtainedValue = entropy.compute(sequence);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldEntropyOfSequenceOf4DifferentNucleotidesWithManyGapsReturn2() {
+        long expectedValue = 2;
+        sequence = new char[][]{{'A', '_', 'G', '_'}, {'_', 'C', 'T', '_'}};
+        double obtainedValue = entropy.compute(sequence);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldEntropyOfSequenceOf8InvertedNucleotidesWithGapsReturn2() {
+        long expectedValue = 2;
+        sequence = new char[][]{{'A', 'G', 'C', 'T', '_', '_', '_'}, {'T', 'C', 'G', 'A', '_', '_', '_'}};
+        double obtainedValue = entropy.compute(sequence);
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldEntropyOfSequenceOf8NoOrderNucleotidesWithInsideReturn2() {
+        long expectedValue = 2;
+        sequence = new char[][]{{'A', '_', 'G', 'C', '_', 'T'}, {'G', '_', 'T', 'A', 'C', '_'}};
+        double obtainedValue = entropy.compute(sequence);
+        assertEquals(expectedValue, obtainedValue);
+    }
 
 }
 
