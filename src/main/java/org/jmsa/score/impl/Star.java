3afab9e8abc00de0ccc95d956b666e7f4f4b3afc
clarajvv <61667090+clarajvv@users.noreply.github.com>
2020-05-14 02:27:21 +0200

    Fixes on Star and created StartTests v1

--- src/main/java/org/jmsa/score/impl/Star.java
+++ src/main/java/org/jmsa/score/impl/Star.java
@@ -3,75 +3,73 @@ package org.jmsa.score.impl;
 import org.jmsa.score.Score;
 import org.jmsa.substitutionmatrix.SubstitutionMatrix;
 
-
-/**
- *
- * grupo 3: irene sanchez, clara jimenez y lucia valverde
- */
+/** grupo 3: irene sanchez, clara jimenez y lucia valverde */
 public class Star implements Score {
   private final SubstitutionMatrix scoreMatrix;
 
-  public Star(SubstitutionMatrix matrix){
+  public Star(SubstitutionMatrix matrix) {
     scoreMatrix = matrix;
   }
 
   public double compute(char[][] sequence) {
     double result = 0;
 
-    //Recorremos las secuencias dentro de sequence. Suponemos que todas las secuencias tienen el mismo tamaño
-    for (int columnSequence = 0; columnSequence<sequence[0].length; columnSequence++){
-      //1. Seleccionamos el aa más repetido de todas las secuencias en la posición (o columna) columnSequence. Para ello creamos dos arrays con la misma longitud
+    // Recorremos las secuencias dentro de sequence. Suponemos que todas las secuencias tienen el
+    // mismo tamaño
+    for (int columnSequence = 0; columnSequence < sequence[0].length; columnSequence++) {
+      // 1. Seleccionamos el aa más repetido de todas las secuencias en la posición (o columna)
+      // columnSequence. Para ello creamos dos arrays con la misma longitud
       // En uno guardamos el char del aa y en el otro su frecuencia
 
       Character[] aaChar = new Character[sequence.length];
       int[] aaFreq = new int[sequence.length];
 
-      //Por cada aa de la columna que estamos leyendo
+      // Por cada aa de la columna que estamos leyendo
       for (char[] chars : sequence) {
         Character aa = chars[columnSequence];
         boolean found = false;
         int index = 0;
-        //Si el aa está en el array, aumentamos la frecuencia
-        while (!found || index < aaFreq.length) {
+        // Si el aa está en el array, aumentamos la frecuencia
+        while (!found && index < aaFreq.length) {
           if (aa.equals(aaChar[index])) {
             aaFreq[index] = aaFreq[index] + 1;
             found = true;
           }
-          index = index + 1;
+          index++;
         }
-        //Si el aa no está en el array, buscamos la primera posicion a cero y añadimos el aa
+        // Si el aa no está en el array, buscamos la primera posicion a cero y añadimos el aa
         if (!found) {
           int index2 = 0;
           boolean notFound = true;
-          while (notFound || index2 < aaFreq.length) {
+          while (notFound && index2 < aaFreq.length) {
             if (aaFreq[index2] == 0) {
               aaChar[index2] = aa;
               aaFreq[index2] = 1;
               notFound = true;
             }
+            index2++;
           }
         }
-
       }
       Character moreRepeated = aaChar[0];
       int value = aaFreq[0];
       for (int i : aaFreq) {
-        //Recorremos el array buscando la posición con la mayor frecuencia y guardamos el aa correspondiente. Este será el centro de la estrella
+        // Recorremos el array buscando la posición con la mayor frecuencia y guardamos el aa
+        // correspondiente. Este será el centro de la estrella
         if (value < i) {
           value = i;
           moreRepeated = aaChar[value];
         }
       }
 
-      //2. Calculamos la puntuación para esta columna
+      // 2. Calculamos la puntuación para esta columna
       for (char[] chars : sequence) {
         double distance = scoreMatrix.getDistance(moreRepeated, chars[columnSequence]);
 
-        //3. Sumamos la distancia al resultado total a devolver
+        // 3. Sumamos la distancia al resultado total a devolver
         result = result + distance;
       }
     }
     return (result);
   }
-
 }
--- src/test/java/org/jmsa/score/impl/StarTest.java
+++ src/test/java/org/jmsa/score/impl/StarTest.java
@@ -1,27 +1,49 @@
 package org.jmsa.score.impl;
 
 import org.jmsa.substitutionmatrix.SubstitutionMatrix;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Case 1:
-    [[A]
-     [B]
-     [C]
- * Case 2:
+ * Assuming we are receiving sequences that containg correct letters.
  *
+ * <p>Case 1: {{'A', 'T', 'C', 'G'}, {'A', 'T', 'C', 'G'}, {'A', 'T', 'C', 'G'}} They are all the
+ * same, so the program should return 3*4*5 = 60
  *
+ * <p>Case 2: {{'A'}, {'A'}, {'T'}} They only differ in one letter, so the program should return
+ * 5+5-4=6
+ *
+ * <p>Case 3: {{'A'}, {'C'}, {'T'}} They are all different, so the program should return 5-4-4 =-3
+ *
+ * <p>Case 4: { {'A', 'T', 'T', 'C', 'G'}, {'A', 'G', 'A', 'C', 'C'}, {'T', 'C', 'G', 'T', 'A'},
+ * {'G', 'G', 'C', 'T', 'A'} } It should return
  */
-
 class StarTest {
-  class DummySubstitutionMatrix implements SubstitutionMatrix{
-    char[][]matrix ;
+  private Star star;
 
+  class DummySubstitutionMatrix implements SubstitutionMatrix {
+    int[][] matrix = {{5, -4, -4, -4}, {-4, 5, -4, -4}, {-4, -4, 5, -4}, {-4, -4, -4, 5}};
+    // creamos una matriz con las distancias
+    private Map<Character, Integer> equivalences = new TreeMap<Character, Integer>();
 
     @Override
     public double getDistance(char char1, char char2) {
-      return 0;
+      // rellenamos el mapa con las equivalencias
+      equivalences.put('A', 0);
+      equivalences.put('C', 1);
+      equivalences.put('G', 2);
+      equivalences.put('T', 3);
+
+      int int1 = equivalences.get(char1); // creamos dos int para transformar los char en int
+      int int2 = equivalences.get(char2);
+
+      double distance = matrix[int1][int2];
+      return distance;
     }
 
     @Override
@@ -30,4 +52,69 @@ class StarTest {
     }
   }
 
+  DummySubstitutionMatrix matrix = new DummySubstitutionMatrix();
+
+  @BeforeEach
+  public void setup() {
+    star = new Star(matrix);
+  }
+
+  @Test
+  public void equalSequencesShouldReturn5LengthSeq() {
+    char[][] sequence = {{'A', 'T', 'C', 'G'}, {'A', 'T', 'C', 'G'}, {'A', 'T', 'C', 'G'}};
+    double expectedValue = 60;
+    double obtainedValue = star.compute(sequence);
+
+    assertEquals(expectedValue, obtainedValue);
+  }
+
+  @Test
+  public void threeUnitarySequencesWithOneDifferenceShouldReturn6() {
+    char[][] sequence = {{'A'}, {'A'}, {'T'}};
+    double expectedValue = 6;
+    double obtainedValue = star.compute(sequence);
+
+    assertEquals(expectedValue, obtainedValue);
+  }
+
+  @Test
+  public void threeUnitarySequencesWithNoEquivalencesShouldReturnMinus3() {
+    char[][] sequence = {{'A'}, {'C'}, {'T'}};
+    double expectedValue = -3;
+    double obtainedValue = star.compute(sequence);
+
+    assertEquals(expectedValue, obtainedValue);
+  }
+
+  @Test
+  public void fourUnitarySequencesWithSameLetterQuantitiesShouldReturn2() {
+    char[][] sequence = {{'A'}, {'A'}, {'T'}, {'T'}};
+    double expectedValue = 2;
+    double obtainedValue = star.compute(sequence);
+
+    assertEquals(expectedValue, obtainedValue);
+  }
+
+  @Test
+  public void threeSequencesWithNoEquivalencesShouldReturnMinus6() {
+    char[][] sequence = {{'A','G'}, {'C','C'}, {'T','T'}};
+    double expectedValue = -6;
+    double obtainedValue = star.compute(sequence);
+
+    assertEquals(expectedValue, obtainedValue);
+  }
+
+  @Test
+  public void exampleSequenceShouldReturn1() {
+    char[][] sequence = {
+      {'A', 'T', 'T', 'C', 'G'},
+      {'A', 'G', 'A', 'C', 'C'},
+      {'T', 'C', 'G', 'T', 'A'},
+      {'G', 'G', 'C', 'T', 'A'}
+    };
+    double expectedValue = 1;
+    double obtainedValue = star.compute(sequence);
+
+    assertEquals(expectedValue, obtainedValue);
+  }
 }
