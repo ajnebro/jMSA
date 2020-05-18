77eee89ad702b15c0d8a0e0847b59d3dbeb4072f
Fiorellaps <fiorellaps@uma.es>
2020-05-18 10:55:22 +0200

    EntropyTest Refactor

--- src/test/java/org/jmsa/score/EntropyTest.java
+++ src/test/java/org/jmsa/score/EntropyTest.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class EntropyTest {
 
     private Entropy entropy;
-    private char[][] sequence;
+    private char[][] multipleSequence;
 
     @BeforeEach
     public void setup() {
@@ -25,32 +25,32 @@ public class EntropyTest {
     @Test
     public void shouldEntropyOfSequenceOf2NucleotidesReturn0() {
         long expectedValue = 0;
-        sequence = new char['A']['A'];
-        double obtainedValue = entropy.compute(sequence);
+        multipleSequence = new char['A']['A'];
+        double obtainedValue = entropy.compute(multipleSequence);
         assertEquals(expectedValue, obtainedValue);
     }
 
     @Test
     public void shouldEntropyOfSequenceOf2DifferentNucleotidesReturns1() {
         long expectedValue = 1;
-        sequence = new char[][]{{'A'}, {'G'}};
-        double obtainedValue = entropy.compute(sequence);
+        multipleSequence = new char[][]{{'A'}, {'G'}};
+        double obtainedValue = entropy.compute(multipleSequence);
         assertEquals(expectedValue, obtainedValue);
     }
 
     @Test
     public void shouldEntropyOfSequenceOf4DifferentNucleotidesReturn2() {
         long expectedValue = 2;
-        sequence = new char[][]{{'A', 'G'}, {'C', 'T'}};
-        double obtainedValue = entropy.compute(sequence);
+        multipleSequence = new char[][]{{'A', 'G'}, {'C', 'T'}};
+        double obtainedValue = entropy.compute(multipleSequence);
         assertEquals(expectedValue, obtainedValue);
     }
 
     @Test
     public void shouldEntropyOfSequenceOf4EqualNucleotidesReturn1() {
         long expectedValue = 1;
-        sequence = new char[][]{{'A', 'G'}, {'A', 'G'}};
-        double obtainedValue = entropy.compute(sequence);
+        multipleSequence = new char[][]{{'A', 'G'}, {'A', 'G'}};
+        double obtainedValue = entropy.compute(multipleSequence);
         assertEquals(expectedValue, obtainedValue);
     }
 
@@ -58,24 +58,24 @@ public class EntropyTest {
     @Test
     public void shouldEntropyOfSequenceOf8NoOrderNucleotidesReturn2() {
         long expectedValue = 2;
-        sequence = new char[][]{{'A', 'G', 'C', 'T'}, {'G', 'T', 'A', 'C'}};
-        double obtainedValue = entropy.compute(sequence);
+        multipleSequence = new char[][]{{'A', 'G', 'C', 'T'}, {'G', 'T', 'A', 'C'}};
+        double obtainedValue = entropy.compute(multipleSequence);
         assertEquals(expectedValue, obtainedValue);
     }
 
     @Test
     public void shouldEntropyOfSequenceOf8EqualNucleotidesReturn2() {
         long expectedValue = 2;
-        sequence = new char[][]{{'A', 'G', 'C', 'T'}, {'A', 'G', 'C', 'T'}};
-        double obtainedValue = entropy.compute(sequence);
+        multipleSequence = new char[][]{{'A', 'G', 'C', 'T'}, {'A', 'G', 'C', 'T'}};
+        double obtainedValue = entropy.compute(multipleSequence);
         assertEquals(expectedValue, obtainedValue);
     }
 
     @Test
     public void shouldEntropyWith8GapsReturn0() {
         long expectedValue = 0;
-        sequence = new char[][]{{'-', '-', '-', '-'}, {'-', '-', '-', '-'}};
-        double obtainedValue = entropy.compute(sequence);
+        multipleSequence = new char[][]{{'-', '-', '-', '-'}, {'-', '-', '-', '-'}};
+        double obtainedValue = entropy.compute(multipleSequence);
         assertEquals(expectedValue, obtainedValue);
 
     }
@@ -83,8 +83,8 @@ public class EntropyTest {
     @Test
     public void shouldEntropyWithGapsOfSequenceOf1NucleotidesReturn0() {
         long expectedValue = 0;
-        sequence = new char[][]{{'A', '-'}, {'A'}};
-        double obtainedValue = entropy.compute(sequence);
+        multipleSequence = new char[][]{{'A', '-'}, {'A'}};
+        double obtainedValue = entropy.compute(multipleSequence);
         assertEquals(expectedValue, obtainedValue);
 
     }
@@ -92,24 +92,24 @@ public class EntropyTest {
     @Test
     public void shouldEntropyOfSequenceOf4DifferentNucleotidesWithGapsReturn2() {
         long expectedValue = 2;
-        sequence = new char[][]{{'A', 'G', '-'}, {'C', 'T', '-'}};
-        double obtainedValue = entropy.compute(sequence);
+        multipleSequence = new char[][]{{'A', 'G', '-'}, {'C', 'T', '-'}};
+        double obtainedValue = entropy.compute(multipleSequence);
         assertEquals(expectedValue, obtainedValue);
     }
 
     @Test
     public void shouldEntropyOfSequenceOf4DifferentNucleotidesWithManyGapsReturn2() {
         long expectedValue = 2;
-        sequence = new char[][]{{'A', '-', 'G', '-'}, {'-', 'C', 'T', '-'}};
-        double obtainedValue = entropy.compute(sequence);
+        multipleSequence = new char[][]{{'A', '-', 'G', '-'}, {'-', 'C', 'T', '-'}};
+        double obtainedValue = entropy.compute(multipleSequence);
         assertEquals(expectedValue, obtainedValue);
     }
 
     @Test
     public void shouldEntropyOfSequenceOf3RepeatedSequencesReturn0() {
         long expectedValue = 0;
-        sequence = new char[][]{{'A', '-'}, {'A'},{'-','A'} };
-        double obtainedValue = entropy.compute(sequence);
+        multipleSequence = new char[][]{{'A', '-'}, {'A'},{'-','A'} };
+        double obtainedValue = entropy.compute(multipleSequence);
         assertEquals(expectedValue, obtainedValue);
     }
 
