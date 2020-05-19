d05258270979667bda369f61c0b6d70c99cdaa3b
marvalpal <0610727402@uma.es>
2020-05-19 11:38:49 +0200

    Commit before merge

--- src/main/java/org/jmsa/substitutionmatrix/impl/PAM250.java
+++ src/main/java/org/jmsa/substitutionmatrix/impl/PAM250.java
@@ -2,19 +2,103 @@ package org.jmsa.substitutionmatrix.impl;
 
 import org.jmsa.substitutionmatrix.SubstitutionMatrix;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.*;
+
 /**
  * Hello world!
  */
 public class PAM250 implements SubstitutionMatrix {
+  private Double gapPenalty = null;
+  private final HashMap<List<String>, Integer> subsMat;
+
+  public PAM250() {
+    this.subsMat = readMatrixFromFile();
+  }
+
+  public PAM250(Double gp) {
+    this.gapPenalty = gp;
+    this.subsMat = readMatrixFromFile();
+  }
+
   public double getDistance(char char1, char char2) {
-    return 0 ;
+    List<String> par = new ArrayList<String>();
+    par.add(Character.toString(char1));
+    par.add(Character.toString(char2));
+    double distance = 0;
+
+    if (!subsMat.containsKey(par)){
+      Collections.reverse(par);
+    }
+    distance = Double.valueOf(subsMat.get(par));
+    return distance;
   }
 
   public char getGapCharacter() {
-    return '-' ;
+    return '-';
   }
 
-  public double getGapPenalty() {
-    return 0 ;
+  public double getGapPenalty() { return getDistance('A', '-'); }
+
+  private HashMap<List<String>, Integer> readMatrixFromFile() {
+    HashMap<List<String>, Integer> substitutionMatrix = new HashMap<List<String>, Integer>();
+    String line;
+    List<String> guide = null;
+    List<String> key1;
+    List<String> key2;
+    int value;
+    try (BufferedReader fileReader = new BufferedReader(new FileReader(String.valueOf(Paths.get("resources/data/PAM250"))))) {
+      while ((line = fileReader.readLine()) != null) {
+        if (!line.startsWith("#")) {
+          List<String> lineMatrix = Arrays.asList(line.trim().split("\\s+"));
+          if (line.startsWith(" ")) {
+            guide = lineMatrix;
+          } else for (int i = 0; i < Objects.requireNonNull(guide).size(); i++) {
+            key1 = new ArrayList<>();
+            key2 = new ArrayList<>();
+            key1.add(lineMatrix.get(0));
+            key1.add(guide.get(i));
+
+            key2.add(guide.get(i));
+            key2.add(lineMatrix.get(0));
+
+            if (!substitutionMatrix.containsKey(key1) && !substitutionMatrix.containsKey(key2)) {
+              boolean k1=(guide.get(i).);
+              if(key1.get(0).contains(Character.toString(getGapCharacter())) && gapPenalty != null){
+                value = gapPenalty.intValue();
+              } else {
+                value = Integer.parseInt(lineMatrix.get(i + 1));
+              }
+              substitutionMatrix.put(key1, value);
+            }
+          }
+        }
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("File: PAM250 is not found");
+      System.exit(1);
+    } catch (IOException e) {
+      System.out.println("An IO error has occured: " + e.getMessage());
+      System.exit(1);
+    }
+    // nothing
+    return substitutionMatrix;
   }
+
+
+
+    public static void main(String[] args) throws IOException {
+        PAM250 p250 = new PAM250(-10D);
+        System.out.println(p250.readMatrixFromFile());
+        System.out.println(p250.getDistance('M', 'A'));
+        System.out.println(p250.getDistance('M', 'A'));
+        System.out.println(p250.getDistance('M', 'A'));
+    }
+
+
+
 }
--- src/test/java/org/jmsa/AppTest.java
+++ src/test/java/org/jmsa/AppTest.java
@@ -1,8 +1,18 @@
 package org.jmsa;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.jmsa.substitutionmatrix.impl.Blosum62;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Unit test for simple App.
@@ -12,9 +22,41 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
+    //@Test
+    //public void shouldAnswerWithTrue()
+    //{
+    //    assertTrue( true );
+    //}
+
+    public static void main( String[] args ) throws IOException
     {
-        assertTrue( true );
+        Blosum62 bs62=new Blosum62();
+        System.out.println(bs62.getDistance('B','A'));
+        /*String filePath = new File("resources\\data\\Blosum62.txt").getAbsolutePath();
+        HashMap<List, String> map = new HashMap<List, String>();
+
+        String line;
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        while ((line = reader.readLine()) != null)
+        {
+            String[] parts = line.split(":", 2);
+            if (parts.length >= 2)
+            {
+                String[] lp=parts[0].split(",");
+                List key = List.of(lp[0].charAt(0),lp[1].charAt(0));
+                String value = parts[1];
+                map.put(key, value);
+            } else {
+                System.out.println("ignoring line: " + line);
+            }
+        }
+        List aux=List.of('B','A');
+        System.out.println(aux + ":" + map.get(aux));
+        for (List key : map.keySet())
+        {
+            System.out.println(key + ":" + map.get(key));
+            System.out.println(key.equals(aux));
+        }
+        reader.close();*/
     }
 }
