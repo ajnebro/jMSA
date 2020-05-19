f4506adbbe38aabe8adb12820ddb61ce2ea65bce
ajnebro <ajnebro@users.noreply.github.com>
2020-05-19 09:31:35 +0200

    Minor refactoring

--- src/main/java/org/jmsa/score/impl/PercentageOfNonGaps.java
+++ src/main/java/org/jmsa/score/impl/PercentageOfNonGaps.java
@@ -3,15 +3,15 @@ package org.jmsa.score.impl;
 import org.jmsa.score.Score;
 
 public class PercentageOfNonGaps implements Score {
-    public double compute(char[][] sequence) {
-        double counterNonGaps = 0;
-        for (int i = 0; i < sequence.length; i++) {
-            for (int j = 0; j < sequence[i].length; j++) {
-                if (sequence[i][j] != '-') {
-                    counterNonGaps += 1;
-                }
-            }
+  public double compute(char[][] sequence) {
+    double counterNonGaps = 0;
+    for (int i = 0; i < sequence.length; i++) {
+      for (int j = 0; j < sequence[i].length; j++) {
+        if (sequence[i][j] != '-') {
+          counterNonGaps += 1;
         }
-        return counterNonGaps / (sequence.length * sequence[0].length) * 100;
+      }
     }
+    return counterNonGaps / (sequence.length * sequence[0].length) * 100;
+  }
 }
--- src/test/java/org/jmsa/PercentageOfNonGapsTest.java
+++ src/test/java/org/jmsa/PercentageOfNonGapsTest.java
@@ -15,48 +15,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PercentageOfNonGapsTest {
 
-    private PercentageOfNonGaps pong;
-    private char[][] sequence;
+  private PercentageOfNonGaps pong;
+  private char[][] sequence;
 
-    @BeforeEach
-    public void setUp() {
-        pong = new PercentageOfNonGaps();
-    }
+  @BeforeEach
+  public void setUp() {
+    pong = new PercentageOfNonGaps();
+  }
 
-    @Test
-    public void thePercentageOfASequenceWithoutGapsIs100() {
-        sequence = new char[][]{{'A', 'G', 'A', 'T'}, {'G', 'G', 'C', 'T'}, {'A', 'G', 'C', 'C'}};
-        assertEquals(100, pong.compute(sequence));
-    }
+  @Test
+  public void thePercentageOfASequenceWithoutGapsIs100() {
+    sequence = new char[][]{{'A', 'G', 'A', 'T'}, {'G', 'G', 'C', 'T'}, {'A', 'G', 'C', 'C'}};
+    assertEquals(100, pong.compute(sequence));
+  }
 
-    @Test
-    public void thePercentageOfASequenceAllGapsIs0() {
-        sequence = new char[][]{{'-', '-', '-'}, {'-', '-', '-'}, {'-', '-', '-'}};
-        assertEquals(0, pong.compute(sequence));
-    }
+  @Test
+  public void thePercentageOfASequenceAllGapsIs0() {
+    sequence = new char[][]{{'-', '-', '-'}, {'-', '-', '-'}, {'-', '-', '-'}};
+    assertEquals(0, pong.compute(sequence));
+  }
 
-    @Test
-    public void thePercentageOfASequenceHalfLettersHalfGapsIs50() {
-        sequence = new char[][]{{'-', 'G', '-', 'T', '-'}, {'A', 'G', '-', '-', 'T'}};
-        assertEquals(50, pong.compute(sequence));
-    }
+  @Test
+  public void thePercentageOfASequenceHalfLettersHalfGapsIs50() {
+    sequence = new char[][]{{'-', 'G', '-', 'T', '-'}, {'A', 'G', '-', '-', 'T'}};
+    assertEquals(50, pong.compute(sequence));
+  }
 
-    @Test
-    public void thePercentageOfASequenceThreeQuartersLettersIs75() {
-        sequence = new char[][]{{'A', 'G', '-', 'T'}, {'G', '-', 'C', 'T'}, {'A', 'G', '-', 'C'}};
-        assertEquals(75, pong.compute(sequence));
-    }
+  @Test
+  public void thePercentageOfASequenceThreeQuartersLettersIs75() {
+    sequence = new char[][]{{'A', 'G', '-', 'T'}, {'G', '-', 'C', 'T'}, {'A', 'G', '-', 'C'}};
+    assertEquals(75, pong.compute(sequence));
+  }
 
-    @Test
-    public void thePercentageOfASequence2GapsAnd10LettersIs83with33() {
-        sequence = new char[][]{{'A', 'G', '-', 'T'}, {'G', 'G', 'C', 'T'}, {'A', 'G', '-', 'C'}};
-        assertEquals(((double) 10 / 12) * 100, pong.compute(sequence));
-    }
+  @Test
+  public void thePercentageOfASequence2GapsAnd10LettersIs83with33() {
+    sequence = new char[][]{{'A', 'G', '-', 'T'}, {'G', 'G', 'C', 'T'}, {'A', 'G', '-', 'C'}};
+    assertEquals(((double) 10 / 12) * 100, pong.compute(sequence));
+  }
 
-    @Test
-    public void thePercentageOfASequence5GapsAnd13LettersIs72with22() {
-        sequence = new char[][]{{'C', 'G'}, {'A', '-'}, {'G', 'T'}, {'-', 'C'}, {'A', '-'}, {'-', 'C'},
-                {'C', 'G'}, {'A', '-'}, {'G', 'T'}};
-        assertEquals(((double) 13 / 18) * 100, pong.compute(sequence));
-    }
+  @Test
+  public void thePercentageOfASequence5GapsAnd13LettersIs72with22() {
+    sequence = new char[][]{{'C', 'G'}, {'A', '-'}, {'G', 'T'}, {'-', 'C'}, {'A', '-'}, {'-', 'C'},
+            {'C', 'G'}, {'A', '-'}, {'G', 'T'}};
+    assertEquals(((double) 13 / 18) * 100, pong.compute(sequence));
+  }
 }
