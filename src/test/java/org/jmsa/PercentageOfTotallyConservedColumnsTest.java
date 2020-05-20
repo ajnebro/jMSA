ea8c489d36c7f584bc0825e0792469e38e39aca8
antoniovar <61683213+antoniovar@users.noreply.github.com>
2020-05-20 17:09:21 +0200

    Add files via upload

--- src/test/java/org/jmsa/PercentageOfTotallyConservedColumnsTest.java
+++ src/test/java/org/jmsa/PercentageOfTotallyConservedColumnsTest.java
@@ -7,15 +7,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PercentageOfTotallyConservedColumnsTest {
-    /*Vamos a hacer una serie de pruebas para comprobar que nuestro programa funciona:
-        1. El porcentaje de una secuencia en la que coinciden todas las columnas es 100.
-        2. El porcentaje de una secuencia en la que no coincide ninguna columa es 0.
-        3. El porcentaje de una secuencia en la que coinciden la mitad de las columnas ha de ser 50.
-        4. El porcentaje de una secuencia en la que coinciden 2 columnas de 7 debe de ser 40.
-        5. Si alguna tiene más columnas que otra se cogerá la más pequeña para asegurar que sean todos los que coincidan
-        para esto vamos a usar una que será mas corta que el resto. Esta más corta no tendrá ninguna en común mientras que
-         las otras más largas si tendrán. Esta prueba ha de devolver 0.
-    * */
+    /* Now lets do som tests to see if our programme works:
+           1. The % of a sequence in which all the columns are the same is 100.
+           2. The % of a sequence in which all the columns are different is 0.
+           3. The % of a sequence in which only half of the columns are the same is 50.
+           4. The % of a sequence in which 2 columns out of 7 are the same is 40.
+       * */
     private char[][] sequence;
     private PercentageOfTotallyConservedColumns porcentajeConservedColumns;
 
@@ -24,31 +21,25 @@ class PercentageOfTotallyConservedColumnsTest {
         porcentajeConservedColumns = new PercentageOfTotallyConservedColumns();
     }
     @Test
-    public void porcentajeCoincidenTodasDevuelve100(){
+    public void allEqualPercentajeReturn100(){
         sequence = new char[][]{{'A', 'G', 'C'}, {'A', 'G', 'C'}, {'A', 'G', 'C'}};
         assertEquals(100, porcentajeConservedColumns.compute(sequence));
 
     }
     @Test
-    public void porcentajeCoincidenNingunaDevuelve0(){
+    public void noMatchReturn0(){
         sequence = new char[][]{{'A', 'C', 'T'}, {'C', 'T', 'A'}, {'T', 'A', 'C'}};
         assertEquals(0, porcentajeConservedColumns.compute(sequence));
     }
     @Test
-    public void porcentajeCoincidenLaMitadDevuelve50(){
+    public void matchHalfReturn50(){
         sequence = new char[][]{{'A', 'C', 'T', 'T'}, {'C', 'C', 'T','G'}, {'T', 'C', 'T', 'G'}};
         assertEquals(50, porcentajeConservedColumns.compute(sequence));
     }
     @Test
-    public void porcentajeCoinciden2de7devuelve2857(){
+    public void match2of5returns2857(){
         sequence = new char[][]{{'A', 'C', 'T', 'T', 'T'}, {'C', 'C', 'T','G', 'T'}, {'T', 'C', 'T', 'G', 'A'}};
         assertEquals(((double)2/5)*100, porcentajeConservedColumns.compute(sequence));
     }
-    @Test
-    public void porcentajeCoinciden0comprobamosLongitudDevuelve0(){
-        sequence = new char[][]{{'A'}, {'C', 'C', 'T','G', 'T'}, {'T', 'C', 'T', 'G', 'A'}};
-        assertEquals(0, porcentajeConservedColumns.compute(sequence));
-    }
-
 
 }
