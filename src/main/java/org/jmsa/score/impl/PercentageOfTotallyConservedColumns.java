d6dc8e688e383ed426be7ad2bf7296175dc83d27
antoniovar <61683213+antoniovar@users.noreply.github.com>
2020-05-20 17:05:43 +0200

    Add files via upload

--- src/main/java/org/jmsa/score/impl/PercentageOfTotallyConservedColumns.java
+++ src/main/java/org/jmsa/score/impl/PercentageOfTotallyConservedColumns.java
@@ -2,11 +2,24 @@ package org.jmsa.score.impl;
 
 import org.jmsa.score.Score;
 
-/**
- * Hello world!
- */
+//We assume they are correctly aligned
 public class PercentageOfTotallyConservedColumns implements Score {
   public double compute(char[][] sequence) {
-    return 0;
+    double counterConservedColumns=0;
+    double lenghtSeq= sequence[0].length;
+    for (int j = 0; j < lenghtSeq; j++){
+        double equalCounter=0;
+        for (int i = 0; i < sequence.length-1; i++) {
+          if (sequence[i][j] == sequence[i + 1][j]) {
+            equalCounter += 1;
+          }
+          if (equalCounter == sequence.length - 1) {
+            counterConservedColumns += 1;
+          }
+        }
+    }
+    return counterConservedColumns/(lenghtSeq)*100;
+
   }
+
 }
