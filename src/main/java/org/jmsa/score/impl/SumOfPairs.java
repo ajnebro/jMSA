0c46b2e56c87331ef6b237191bdbcc93cb3c3f83
Juan <juansanchezrodriguez1999@uma.es>
2020-05-20 17:24:49 +0200

    Refactorizacion con before each y arreglos en el codigo

--- src/main/java/org/jmsa/score/impl/SumOfPairs.java
+++ src/main/java/org/jmsa/score/impl/SumOfPairs.java
@@ -7,25 +7,27 @@ import org.jmsa.substitutionmatrix.SubstitutionMatrix;
  */
 public class SumOfPairs implements Score {
     private SubstitutionMatrix matrix;
+
     public SumOfPairs(SubstitutionMatrix m) {
         matrix = m;
     }
+
     public double compute(char[][] sequence) {
         double totalSum = 0;
         for (int c = 0; c < sequence[0].length; c++) {
             totalSum += sumCol(sequence,c);
             }
         return totalSum;
-        }
-    public double sumCol(char[][] sequence,int columna){
+    }
 
+    public double sumCol(char[][] sequence,int column){
         double columnSum=0;
         for (int f = 0; f < sequence.length - 1; f++) {
             for (int p = f + 1; p < sequence.length; p++) {
-                columnSum += matrix.getDistance(sequence[f][columna], sequence[p][columna]);
+                columnSum += matrix.getDistance(sequence[f][column], sequence[p][column]);
             }
         }
         return columnSum;
-  }
+    }
 
 }
--- src/test/java/org/jmsa/score/SumOfPairsTest.java
+++ src/test/java/org/jmsa/score/SumOfPairsTest.java
@@ -1,10 +1,8 @@
 package org.jmsa.score;
-
 import org.jmsa.score.impl.SumOfPairs;
 import org.jmsa.substitutionmatrix.SubstitutionMatrix;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -13,16 +11,11 @@ class SumOfPairsTest {
 
     private SumOfPairs sum;
 
-    @Test
-    public void testneg27() {
-        char[][] matrix = {{'A', 'A','C'}, {'A', 'T','-'},{'C', '-','A'}};
-        /*        {'A', 'A','C'},  +5 -4 -4
-                  {'A', 'T','-'},  -4 -4 -4
-                  {'C', '-','A'}   -4 -4 -4
-                                                +5 - 4*8 = -27*/
+    @BeforeEach
+    //Before each test we'll create a SumOfPairs Object and a SubstitutionMatrix with mock
+    public void setup() {
         SubstitutionMatrix substitutionMatrix = mock(SubstitutionMatrix.class);
-        SumOfPairs sum = new SumOfPairs(substitutionMatrix);
-
+        sum = new SumOfPairs(substitutionMatrix);
         when(substitutionMatrix.getDistance('A','A')).thenReturn(5.0);
         when(substitutionMatrix.getDistance('A','T')).thenReturn(-4.0);
         when(substitutionMatrix.getDistance('A','G')).thenReturn(-4.0);
@@ -48,133 +41,42 @@ class SumOfPairsTest {
         when(substitutionMatrix.getDistance('-','T')).thenReturn(-4.0);
         when(substitutionMatrix.getDistance('-','G')).thenReturn(-4.0);
         when(substitutionMatrix.getDistance('-','C')).thenReturn(-4.0);
-
-        double expectedValue = -27;
-        double obtainedValue =sum.compute(matrix);
-        assertEquals(expectedValue, obtainedValue);
-
     }
+
     @Test
-    public void test0() {
-        char[][] matrix = {{'A', 'G','G'}, {'A', 'C','T'},{'A', 'G', '-'}};
-        /*      {'A', 'G', 'G'}, AT AT TT       +5 +5 +5
-                {'A', 'C', 'T'}, G- GG -G       -4 +5 -4
+    /*Example:  {'A', 'G', 'G'}, AT AT TT       +5 +5 +5
+                {'A', 'C', 'G'}, G- GG -G       -4 +5 +5
                 {'A', 'G', '-'}  -A -- A-       -4 -4 -4
-                                                                +5*4 -4*5=0*/
-        SubstitutionMatrix substitutionMatrix = mock(SubstitutionMatrix.class);
-        SumOfPairs sum = new SumOfPairs(substitutionMatrix);
-
-        when(substitutionMatrix.getDistance('A','A')).thenReturn(5.0);
-        when(substitutionMatrix.getDistance('A','T')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('A','G')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('A','C')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('A','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('T','T')).thenReturn(5.0);
-        when(substitutionMatrix.getDistance('T','A')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('T','G')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('T','C')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('T','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('G','G')).thenReturn(5.0);
-        when(substitutionMatrix.getDistance('G','A')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('G','T')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('G','C')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('G','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('C','C')).thenReturn(5.0);
-        when(substitutionMatrix.getDistance('C','A')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('C','T')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('C','G')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('C','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','A')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','T')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','G')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','C')).thenReturn(-4.0);
-
-        double expectedValue = 0;
+                Sum of pair = 5*5 -4*4=9                 */
+    public void shouldSumOfPairsGeneralCaseWithGapsAlignedAndNotAlignedPairsReturnTheRightValue9() {
+        char[][] matrix = {{'A', 'G','G'}, {'A', 'C','G'},{'A', 'G', '-'}};
+        double expectedValue = 9;
         double obtainedValue =sum.compute(matrix);
         assertEquals(expectedValue, obtainedValue);
-
     }
+
     @Test
-    public void allAlignment() {
-        char[][] matrix = {{'A', 'T','C'}, {'A', 'T','C'},{'A', 'T', 'C'}};
-        /*      {'A', 'T', 'C'}, AA AA AA       +5 +5 +5
+    /*Example:  {'A', 'T', 'C'}, AA AA AA       +5 +5 +5
                 {'A', 'T', 'C'}, AA AA AA       +5 +5 +5
                 {'A', 'T', 'C'}  AA AA AA       +5 +5 +5
-                                                                +5*9=45*/
-        SubstitutionMatrix substitutionMatrix = mock(SubstitutionMatrix.class);
-        SumOfPairs sum = new SumOfPairs(substitutionMatrix);
-
-        when(substitutionMatrix.getDistance('A','A')).thenReturn(5.0);
-        when(substitutionMatrix.getDistance('A','T')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('A','G')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('A','C')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('A','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('T','T')).thenReturn(5.0);
-        when(substitutionMatrix.getDistance('T','A')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('T','G')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('T','C')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('T','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('G','G')).thenReturn(5.0);
-        when(substitutionMatrix.getDistance('G','A')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('G','T')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('G','C')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('G','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('C','C')).thenReturn(5.0);
-        when(substitutionMatrix.getDistance('C','A')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('C','T')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('C','G')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('C','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','A')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','T')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','G')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','C')).thenReturn(-4.0);
-
+                Sum of pair = 5*9=45                      */
+    public void shouldATotallyAlignedMSAReturnTheRightValue45() {
+        char[][] matrix = {{'A', 'T','C'}, {'A', 'T','C'},{'A', 'T', 'C'}};
         double expectedValue = 45;
         double obtainedValue =sum.compute(matrix);
         assertEquals(expectedValue, obtainedValue);
-
     }
+
     @Test
-    public void allGaps() {
-        char[][] matrix = {{'-', '-','-'}, {'-', '-','-'},{'-', '-', '-'}};
-        /*      {'-', '-', '-'}, -- -- --       -4 -4 -4
+    /*Example:  {'-', '-', '-'}, -- -- --       -4 -4 -4
                 {'-', '-', '-'}, -- -- --       -4 -4 -4
                 {'-', '-', '-'}  -- -- --       -4 -4 -4
-                                                                -4*9=-36*/
-        SubstitutionMatrix substitutionMatrix = mock(SubstitutionMatrix.class);
-        SumOfPairs sum = new SumOfPairs(substitutionMatrix);
-
-        when(substitutionMatrix.getDistance('A','A')).thenReturn(5.0);
-        when(substitutionMatrix.getDistance('A','T')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('A','G')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('A','C')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('A','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('T','T')).thenReturn(5.0);
-        when(substitutionMatrix.getDistance('T','A')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('T','G')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('T','C')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('T','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('G','G')).thenReturn(5.0);
-        when(substitutionMatrix.getDistance('G','A')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('G','T')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('G','C')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('G','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('C','C')).thenReturn(5.0);
-        when(substitutionMatrix.getDistance('C','A')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('C','T')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('C','G')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('C','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','-')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','A')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','T')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','G')).thenReturn(-4.0);
-        when(substitutionMatrix.getDistance('-','C')).thenReturn(-4.0);
-
+                Sum of pair = -4*9=-36                    */
+    public void shouldATotallyGapsMSAReturnTheRightValueNegative36() {
+        char[][] matrix = {{'-', '-','-'}, {'-', '-','-'},{'-', '-', '-'}};
         double expectedValue = -36;
         double obtainedValue =sum.compute(matrix);
         assertEquals(expectedValue, obtainedValue);
-
     }
+
 }
