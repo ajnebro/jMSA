96cf7e7c19f2b8016c73396d2b627497a2f58fb3
Juan Antonio Herrera Conde <jaherreraconde@MacBook-Pro-de-Juan-JR.local>
2020-05-21 16:25:54 +0200

    Ejemplo Grupo 1

--- src/main/java/org/jmsa/examples/Example1.java
+++ src/main/java/org/jmsa/examples/Example1.java
@@ -1,4 +1,45 @@
 package org.jmsa.examples;
 
+import org.jmsa.util.ReadMSA;
+
+import org.jmsa.score.impl.PercentageOfNonGaps;
+import org.jmsa.score.impl.Entropy;
+import org.jmsa.score.impl.PercentageOfTotallyConservedColumns;
+import org.jmsa.score.impl.Star;
+import org.jmsa.score.impl.SumOfPairs;
+import org.jmsa.substitutionmatrix.impl.PAM250;
+import org.jmsa.substitutionmatrix.impl.Blosum62;
+
+
+import java.io.FileNotFoundException;
+
+
 public class Example1 {
+
+    public static void main(String[]args) throws FileNotFoundException {
+
+        PAM250 p = new PAM250();
+        Blosum62 b = new Blosum62();
+
+        ReadMSA r = new ReadMSA();
+
+        Entropy e = new Entropy();
+        PercentageOfTotallyConservedColumns pt = new PercentageOfTotallyConservedColumns();
+        PercentageOfNonGaps pn = new PercentageOfNonGaps();
+        Star stp = new Star(p);
+        Star stb = new Star(b);
+        SumOfPairs spp = new SumOfPairs(p);
+        SumOfPairs spb = new SumOfPairs(b);
+
+        char [][] secuencia = r.readSequenceFromFastaFile("src/main/java/org/jmsa/examples/hemoglobina_alineada.txt");
+
+        System.out.println ("Entropy score: " + e.compute(secuencia));
+        System.out.println ("Percentaje of Totally Conserved Columns score: " + pt.compute(secuencia));
+        System.out.println ("Percentage on Non Gaps score: " + pn.compute(secuencia));
+        System.out.println ("Star score (PAM250): " + stp.compute(secuencia));
+        System.out.println ("Star score (Blosum62): " + stb.compute(secuencia));
+        System.out.println ("Sum of pairs score (PAM250): " + spp.compute(secuencia));
+        System.out.println ("Sum of pairs score (Blosum62): " + spb.compute(secuencia));
+
+    }
 }
